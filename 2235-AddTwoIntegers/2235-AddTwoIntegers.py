1b68deb37c728fa5a460d7a4733d7d932dcbf7bf
Maya Oum <maya.oum02@gmail.com>
2025-04-19 14:28:51 -0400

    Create file

---
+++ 2235-AddTwoIntegers/2235-AddTwoIntegers.py
@@ -0,0 +1,11 @@
+# Last updated: 4/19/2025, 2:28:50 PM
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+
+        return num1 + num2
+        
